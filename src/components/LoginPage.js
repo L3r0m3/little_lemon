e1323eef92cf27d2daaf280b224890c454b5863c
L3r0m3 <lerome_brown@yahoo.com>
2023-07-07 11:00:15 +0200

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	new file:   src/Icons/SocialIcon.js 	new file:   src/components/LoginPage.js

---
+++ src/components/LoginPage.js
@@ -0,0 +1,106 @@
+import { React, useState } from "react";
+import Nav from '../components/Nav';
+import {
+    Box,
+    Heading,
+    HStack,
+    Text,
+    VStack,
+    Stack,
+    Link,
+    Button,
+    FormControl,
+    FormLabel,
+    Input,
+    FormHelperText,
+    FormErrorMessage,
+    ChakraProvider,
+    InputGroup,
+    InputRightElement
+    } from "@chakra-ui/react";
+    import  {GoogleIcon, FacebookIcon, AppleIcon, LinkedinIcon} from '../Icons/SocialIcon';
+
+const LoginPage = () => {
+        const [input, setInput] = useState('');
+        const handleInputChange = (e) => setInput(e.target.value);
+        const isError = input === '';
+        const [show, setShow] = useState(false)
+        const handleClick = () => setShow(!show)
+
+    return (
+        <ChakraProvider>
+            <Nav />
+            <Stack
+             display='grid'
+             bg='#495E57'
+             py={100}
+             spacing={8}
+             justify={['center', 'center', 'center']}
+            >
+                <Heading ml={120} >Login</Heading>
+                <HStack display='flex' justify='center'>
+                    <Link><Button bg='#F4CE14'>Login</Button></Link>
+                    <Link><Button bg='#EE9972'>Signup</Button></Link>
+                </HStack>
+                <Box
+                    border='1px'
+                    borderColor='gray.200'
+                    p={3}
+                    rounded="md"
+                    w="100%"
+                    maxW={['320', '560']}
+                    >
+                    <VStack>
+                    <FormControl>
+                        <FormLabel color='white'>Email</FormLabel>
+                        <Input
+                         bg='white'
+                         type='email'
+                         placeholder="Enter email"
+                         value={input}
+                         onChange={handleInputChange}
+                        />
+                        {!isError ? (
+                            <FormHelperText>
+                                Enter email
+                            </FormHelperText>
+                        ) : (
+                            <FormErrorMessage>Email is required.</FormErrorMessage>
+                        )}
+                        </FormControl>
+                        <FormControl>
+                            <FormLabel color='white'>Password</FormLabel>
+                        <InputGroup size='md'>
+                            <Input
+                                pr='4.5rem'
+                                type={show ? 'text' : 'password'}
+                                placeholder='Enter password'
+                                bg='white'
+                            />
+                            <InputRightElement width='4.5rem'>
+                                <Button h='1.75rem' size='sm' onClick={handleClick}>
+                                {show ? 'Hide' : 'Show'}
+                                </Button>
+                            </InputRightElement>
+                        </InputGroup>
+                            <Link color='#EE9972' textAlign='left' fontSize={13}>Forgot password?</Link>
+                        </FormControl>
+                    </VStack>
+                </Box>
+                <Button bg='#F4CE14'>Login</Button>
+                <HStack justify='center'>
+                    <Text>Create account</Text>
+                    <Link color='#EE9972'>Signup now</Link>
+                </HStack>
+                <HStack justify='center'>
+                    <GoogleIcon fontSize={30} />
+                    <FacebookIcon ml={3} fontSize={30} />
+                    <LinkedinIcon ml={3} fontSize={30} />
+                    <AppleIcon ml={3} fontSize={30} />
+                </HStack>
+            </Stack>
+        </ChakraProvider>
+    )
+}
+
+export default LoginPage;
